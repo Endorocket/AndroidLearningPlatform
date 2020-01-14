15c6a7618889b8b1e0e9161b302d852236897ff1
Endorocket <43682886+Endorocket@users.noreply.github.com>
2020-01-14 23:58:57 +0100

    Music Learning Page

--- app/src/main/java/com/example/androidlearningplatform/sections/FilmMusicFragment.java
+++ app/src/main/java/com/example/androidlearningplatform/sections/FilmMusicFragment.java
@@ -1,5 +1,6 @@
 package com.example.androidlearningplatform.sections;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,83 @@ public class FilmMusicFragment extends Fragment {
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_film_music, container, false);
+
+    final View btnPlayDarkKnightSound = rootView.findViewById(R.id.btnPlayDarkKnightSound);
+    final View btnStopDarkKnightSound = rootView.findViewById(R.id.btnStopDarkKnightSound);
+    final View btnPlayGladiatorSound = rootView.findViewById(R.id.btnPlayGladiatorSound);
+    final View btnStopGladiatorSound = rootView.findViewById(R.id.btnStopGladiatorSound);
+    final View btnPlayInterstellarSound = rootView.findViewById(R.id.btnPlayInterstellarSound);
+    final View btnStopInterstellarSound = rootView.findViewById(R.id.btnStopInterstellarSound);
+
+    final MediaPlayer mpDarkKnight = MediaPlayer.create(getContext(), R.raw.dark_knight_sound);
+    final MediaPlayer mpGladiator = MediaPlayer.create(getContext(), R.raw.gladiator_sound);
+    final MediaPlayer mpInterstellar = MediaPlayer.create(getContext(), R.raw.interstellar_music);
+
+    btnPlayDarkKnightSound.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (mpGladiator.isPlaying())
+          mpGladiator.pause();
+
+        if (mpInterstellar.isPlaying())
+          mpInterstellar.pause();
+
+        if (!mpDarkKnight.isPlaying())
+          mpDarkKnight.start();
+      }
+    });
+
+    btnStopDarkKnightSound.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (mpDarkKnight.isPlaying())
+          mpDarkKnight.pause();
+      }
+    });
+
+    btnPlayGladiatorSound.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (mpDarkKnight.isPlaying())
+          mpDarkKnight.pause();
+
+        if (mpInterstellar.isPlaying())
+          mpInterstellar.pause();
+
+        if (!mpGladiator.isPlaying())
+          mpGladiator.start();
+      }
+    });
+
+    btnStopGladiatorSound.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (mpGladiator.isPlaying())
+          mpGladiator.pause();
+      }
+    });
+
+    btnPlayInterstellarSound.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (mpDarkKnight.isPlaying())
+          mpDarkKnight.pause();
+
+        if (mpGladiator.isPlaying())
+          mpGladiator.pause();
+
+        if (!mpInterstellar.isPlaying())
+          mpInterstellar.start();
+      }
+    });
+
+    btnStopInterstellarSound.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (mpInterstellar.isPlaying())
+          mpInterstellar.pause();
+      }
+    });
 
     return rootView;
   }
